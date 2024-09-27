40e919453f6784ae940afb55f1bf7ec985a6f8ce
ashishsfdc123 <ashishsfdc123@gmail.com>
2024-09-27 23:11:21 +0530

    commit this

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,27 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/**
+ * Configure Prettier
+ * Configure Global Snippets
+ */
+
+const x = 23;
+if (x === 23) {
+  console.log(23);
+}
+
+const calcAge = (birthYear) => 2024 - birthYear;
+console.log(calcAge(1990)); //34
+
+/**
+ * Learn how to code
+ * There should be clear goal at the beginning of learning
+ * a lot of practice should be there
+ * learn in group
+ * dont loose motivation, dont be frustrated
+ *
+ * What we need to do:
+ *
+ * 1. Set a goal: specific, measureable, time based
+ */
